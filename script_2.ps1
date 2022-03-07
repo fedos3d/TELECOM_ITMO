21ae3f3a5f301cc52d6aa505fe061e9f448fa3b3
Fedor Anikeev <37173454+fedos3d@users.noreply.github.com>
2022-03-07 14:48:59 +0300

    Update script_2.ps1

--- script_2.ps1
+++ script_2.ps1
@@ -2,6 +2,13 @@ $param = Read-Host -Prompt 'Enter Auto or Manual: '
 while (($param -ne 'Auto') -and ($param -ne 'Manual')) {
     $param = Read-Host -Prompt 'Enter Auto or Manual: '
 }
+$info = Read-Host -Prompt 'Do you want to list additional info? (enter YES or NO): '
+while (($param -ne 'YES') -and ($param -ne 'NO')) {
+    $info = Read-Host -Prompt 'Do you want to list additional info? (enter YES or NO): '
+}
+if ($info -eq 'YES') {
+     Get-NetAdapter | select InterfaceDescription, Status, FullDuplex
+}
 if ($param -eq 'Auto') {
     netsh interface ipv4 set address name="Ethernet0" source=dhcp
     netsh interface ipv4 set dnsservers "Ethernet0" source=dhcp
