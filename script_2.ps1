5afab39ddab717908ee2a5c0ed92493fc5645534
Fedor Anikeev <37173454+fedos3d@users.noreply.github.com>
2022-03-09 09:59:09 +0300

    Update script_2.ps1

--- script_2.ps1
+++ script_2.ps1
@@ -10,10 +10,10 @@ if ($info -eq 'YES') {
      Get-NetAdapter | select InterfaceDescription, Status, FullDuplex, Speed
 }
 if ($param -eq 'Auto') {
-    netsh interface ipv4 set address name="Ethernet0" source=dhcp
-    netsh interface ipv4 set dnsservers "Ethernet0" source=dhcp
+    netsh interface ipv4 set address name="Ethernet 2" source=dhcp
+    netsh interface ipv4 set dnsservers "Ethernet 2" source=dhcp
 }
 if ($param -eq 'Manual') {
-    netsh interface ipv4 set address name="Ethernet0" static address=192.168.1.10 mask=255.255.255.0 gateway=192.168.1.1
-    netsh interface ipv4 set dns "Ethernet0" static 8.8.8.8
+    netsh interface ipv4 set address name="Ethernet 2" static address=192.168.0.193 mask=255.255.255.0 gateway=192.168.0.1
+    netsh interface ipv4 set dns "Ethernet 2" static 1.1.1.1
 }
